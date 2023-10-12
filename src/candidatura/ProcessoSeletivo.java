bb14da2d3aacd96880fa3aa98553f85d88586f32
kaflo <kaiodavy7@gmail.com>
2023-10-12 12:07:23 -0300

    metodo de ligar para os candidatos selecionados construido

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,38 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        imprimirSelecionados();
+        String[] candidatos = {"Kaio", "João", "Davi", "Matheus", "Gabriela"};
+        for (String candidato : candidatos) {
+            case4(candidato);
+        }
+    }
+
+    static void case4(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu=false;
+        do {
+            atendeu= atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("Contato realizado com sucesso!");
+            }
+
+        } while(continuarTentando && tentativasRealizadas<3);
+        if(atendeu) {
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + "° tentativa");
+        } else {
+            System.out.println("Não conseguimos contato com " + candidato + ", número máximo de tentativas (" + tentativasRealizadas + ") realizadas");
+        }
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados() {
